19ed7b66b4abce149a47adb316c1b84db84be366
bissness11 <154408458+bissness11@users.noreply.github.com>
2024-08-18 16:20:36 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "6534707"))
+API_HASH = getenv("API_HASH", "4bcc61d959a9f403b2f20149cbbe627a")
+BOT_TOKEN = getenv("BOT_TOKEN", "5502855202:AAGW3MYUov5Gb2wWgpcaNTgxDjZ98IFh4P8")
+OWNER_ID = int(getenv("OWNER_ID", "1430593323"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://bissness958:lsYsQQByKXaOPRUP@cluster0.dj7yv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1001843564893"))
+FORCESUB = getenv("FORCESUB", "Animecolony")
